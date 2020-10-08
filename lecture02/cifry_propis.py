94f90e4c6a419ac1f47f8fa2b91ef0db2388d5de
oreshka06 <vorishka2012@gmail.com>
2020-10-09 00:19:57 +0700

    lecture02/

---
+++ lecture02/cifry_propis.py
@@ -0,0 +1,247 @@
+a = input('Вход (от 0 до 99): ') # по умолчанию вводится строка
+a = int(a)
+
+# Я не поняла, как сделать проверку, это мои наметки, как можно было бы сделать
+
+# Я хотела сделать так, чтобы True был только диапазон от 0 до 99,
+# а всё остальное было False. Но я не поняла, как присваивать False всему остальному
+
+# b = range(0, 100)
+# b = True 
+
+# if a == b:
+    # a = int(a)
+# else: print ('Вы должны ввести число')
+
+zero = 'ноль'
+one = 'один'
+two = 'два'
+three = 'три'
+four = 'четыре'
+five = 'пять'
+six = 'шесть'
+seven = 'семь'
+eight = 'восемь'
+nine = 'девять'
+ten = 'десять'
+eleven = 'одиннадцать'
+twelve = 'двенадцать'
+thirteen = 'тринадцать'
+fourteen = 'четырнадцать'
+fifteen = 'пятнадцать'
+sixteen = 'шестнадцать'
+seventeen = 'семнадцать'
+eighteen = 'восемнадцать'
+nineteen = 'девятнадцать'
+twenty = 'двадцать ' #пробел после этих слов для того, чтобы строки не склеивались, а были с пробелом
+thirty = 'тридцать '
+fourty = 'сорок '
+fifty = 'пятьдесят '
+sixty = 'шестьдесят '
+seventy = 'семьдесят '
+eighty = 'восемьдесят '
+ninety = 'девяносто '
+
+# Долгая и нудная проверка (не знаю, как сократить)
+
+if a == 0:
+    print(zero)
+elif a == 1:
+    print(one)
+elif a == 2:
+    print(two)
+elif a == 3:
+    print(three)
+elif a == 4:
+    print(four)
+elif a == 5:
+    print(five)
+elif a == 6:
+    print(six)
+elif a == 7:
+    print(seven)
+elif a == 8:
+    print(eight)
+elif a == 9:
+    print(nine)
+elif a == 10:
+    print(ten)
+elif a == 11:
+    print(eleven)
+elif a == 12:
+    print(twelve)
+elif a == 13:
+    print(thirteen)
+elif a == 14:
+    print(fourteen)
+elif a == 15:
+    print(fifteen)
+elif a == 16:
+    print(sixteen)
+elif a == 17:
+    print(seventeen)
+elif a == 18:
+    print(eighteen)
+elif a == 19:
+    print(nineteen)
+elif a == 20:
+    print(twenty)
+elif a == 21:
+    print(twenty + one)
+elif a == 22:
+    print(twenty + two)
+elif a == 23:
+    print(twenty + three)
+elif a == 24:
+    print(twenty + four)
+elif a == 25:
+    print(twenty + five)
+elif a == 26:
+    print(twenty + six)
+elif a == 27:
+    print(twenty + seven)
+elif a == 28:
+    print(twenty + eight)
+elif a == 29:
+    print(twenty + nine)
+elif a == 30:
+    print(thirty)
+elif a == 31:
+    print(thirty + one)
+elif a == 32:
+    print(thirty + two)
+elif a == 33:
+    print(thirty + three)
+elif a == 34:
+    print(thirty + four)
+elif a == 35:
+    print(thirty + five)
+elif a == 36:
+    print(thirty + six)
+elif a == 37:
+    print(thirty + seven)
+elif a == 38:
+    print(thirty + eight)
+elif a == 39:
+    print(thirty + nine)    
+elif a == 40:
+    print(fourty)
+elif a == 41:
+    print(fourty + one)
+elif a == 42:
+    print(fourty + two)
+elif a == 43:
+    print(fourty + three)
+elif a == 44:
+    print(fourty + four)
+elif a == 45:
+    print(fourty + five)
+elif a == 46:
+    print(fourty + six)
+elif a == 47:
+    print(fourty + seven)
+elif a == 48:
+    print(fourty + eight)
+elif a == 49:
+    print(fourty + nine)   
+elif a == 50:
+    print(fifty)
+elif a == 51:
+    print(fifty + one)
+elif a == 52:
+    print(fifty + two)
+elif a == 53:
+    print(fifty + three)
+elif a == 54:
+    print(fifty + four)
+elif a == 55:
+    print(fifty + five)
+elif a == 56:
+    print(fifty + six)
+elif a == 57:
+    print(fifty + seven)
+elif a == 58:
+    print(fifty + nine)
+elif a == 59:
+    print(fifty + nine) 
+elif a == 60:
+    print(sixty)
+elif a == 61:
+    print(sixty + one)
+elif a == 62:
+    print(sixty + two)
+elif a == 63:
+    print(sixty + three)
+elif a == 64:
+    print(sixty + four)
+elif a == 65:
+    print(sixty + five)
+elif a == 66:
+    print(sixty + six)
+elif a == 67:
+    print(sixty + seven)
+elif a == 68:
+    print(sixty + eight)
+elif a == 69:
+    print(sixty + nine) 
+elif a == 70:
+    print(seventy)
+elif a == 71:
+    print(seventy + one)
+elif a == 72:
+    print(seventy + two)
+elif a == 73:
+    print(seventy + three)
+elif a == 74:
+    print(seventy + four)
+elif a == 75:
+    print(seventy + five)
+elif a == 76:
+    print(seventy + six)
+elif a == 77:
+    print(seventy + seven)
+elif a == 78:
+    print(seventy + eight)
+elif a == 79:
+    print(seventy + nine) 
+elif a == 80:
+    print(eighty)
+elif a == 81:
+    print(eighty + one)
+elif a == 82:
+    print(eighty + two)
+elif a == 83:
+    print(eighty + three)
+elif a == 84:
+    print(eighty + four)
+elif a == 85:
+    print(eighty + five)
+elif a == 86:
+    print(eighty + six)
+elif a == 87:
+    print(eighty + seven)
+elif a == 88:
+    print(eighty + eight)
+elif a == 89:
+    print(eighty + nine) 
+elif a == 90:
+    print(ninety)
+elif a == 91:
+    print(ninety + one)
+elif a == 92:
+    print(ninety + two)
+elif a == 93:
+    print(ninety + three)
+elif a == 94:
+    print(ninety + four)
+elif a == 95:
+    print(ninety + five)
+elif a == 96:
+    print(ninety + six)
+elif a == 97:
+    print(ninety + seven)
+elif a == 98:
+    print(ninety + eight)
+elif a == 99:
+    print(ninety + nine) 
+else: print('Неверный ввод данных')
